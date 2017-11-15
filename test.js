26441f216708ae34404998e21b3ce81009b3108c
cag2050 <cag2050@163.com>
2017-11-15 13:22:35 +0800

    m

---
+++ test.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+
+let content = fs.readFileSync('/Users/cag/Downloads/Test.txt','utf-8')
+// console.log(/\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDE4F]/.test(content))
+console.log('全局匹配到表情的数量：')
+console.log(content.match(/\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDE4F]/g).length)
+console.log('全局匹配到表情为：')
+console.log(content.match(/\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDE4F]/g))
